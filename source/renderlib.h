620152742a0ee74a5766d83a3426234e29a84633
Jose Esteve <jose@joesfer.com>
2012-01-02 19:15:26 +0000

    Clarified license terms

--- source/renderlib.h
+++ source/renderlib.h
@@ -1,3 +1,10 @@
+/* 
+	================================================================================
+	Copyright (c) 2012, Jose Esteve. http://www.joesfer.com
+	This software is released under the LGPL-3.0 license: http://www.opensource.org/licenses/lgpl-3.0.html	
+	================================================================================
+*/
+
 #pragma once
 
 /// Main header
